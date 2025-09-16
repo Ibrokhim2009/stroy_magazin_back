3ed729e530bf9a8779dd11fb97d181d787411927
ibrokhim2009 <ibrokhimakbarib@gmail.com>
2025-09-16 15:18:07 +0500

    project created

---
+++ core/auth_models.py
@@ -0,0 +1,87 @@
+from functools import lru_cache
+from django.core.cache import cache
+from django.utils.translation import gettext_lazy as _
+import urllib.request
+import json
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone, username, password=None, **extra_fields):
+        if not phone:
+            raise ValueError(_('The Phone field must be set'))
+        if not username:
+            raise ValueError(_('The Username field must be set'))
+
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', False)
+
+        user = self.model(phone=phone, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('user_type', 2)  # admin
+
+        return self.create_user(phone, username, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_("Ism familiya"), max_length=50)
+    phone = models.CharField(_("Telefon raqam"), max_length=12, unique=True)
+    place = models.CharField(max_length=512)
+    email = models.EmailField()
+    user_type = models.SmallIntegerField(default=1, choices=[
+        (1, 'user'),
+        (2, 'admin')
+    ])
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    
+    objects = CustomUserManager()
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['username', 'user_type']
+
+    def __str__(self):
+        return self.username
+
+    @lru_cache(maxsize=1)
+    def get_exchange_rates(self):
+        rates = cache.get("exchange_rates")
+        if not rates:
+            try:
+                with urllib.request.urlopen("https://cbu.uz/oz/arkhiv-kursov-valyut/json/") as response:
+                    if response.status == 200:
+                        data = json.loads(response.read().decode('utf-8'))
+                        usd = next(item for item in data if item["Ccy"] == "USD")["Rate"]
+                        rub = next(item for item in data if item["Ccy"] == "RUB")["Rate"]
+
+                        rates = {
+                            "USD": float(usd),
+                            "RUB": float(rub),
+                            "UZS": 1.0
+                        }
+                        cache.set("exchange_rates", rates, 60 * 60)
+            except Exception:
+                rates = {
+                    "USD": 12800.0,
+                    "RUB": 163.0,
+                    "UZS": 1.0
+                }
+        return rates
+
+    def calculate_cart(self):
+        carts = self.carts.filter(status=True)
+        total_balance = 0.0
+        rates = self.get_exchange_rates()
+        for cart in carts:
+            cart_total = cart.total_price
+            price_type = cart.product.price_type
+            total_balance += cart_total * rates[price_type]
+        usd_total = total_balance / rates["UZS"]
+        return f"{usd_total:.2f} sum"
---
+++ core/models.py
@@ -0,0 +1,201 @@
+from django.db import models
+
+from core.auth_models import User
+import os
+import uuid
+# Create your models here.
+
+def product_image_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    new_filename = f"{uuid.uuid4()}.{ext}"
+    return os.path.join("products", new_filename)
+
+
+
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
+    image = models.ImageField(upload_to="categories/", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_level(self) -> int:
+        level = 1
+        parent = self.parent
+        while parent:
+            level += 1
+            parent = parent.parent
+        return level
+
+    def clean(self):
+        level = self.get_level()
+        if level > 3:
+            self.parent = None
+        if level > 1 and self.image:
+            self.image = None
+
+
+
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    price = models.IntegerField(default=0)
+    discount = models.IntegerField(default=0)
+    price_type = models.CharField(max_length=3, choices=[
+        ("UZS", "O'zbek so'mi"),
+        ("USD", "Aqsh Dollori"),
+        ("RUB", "Rossiya Rubli"),
+    ], default="UZS")
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name="product_category")
+    discription = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, auto_created=False)
+    updated_at = models.DateField(auto_now_add=False, auto_now=True)
+    extra = models.JSONField(default=dict)
+    
+    def get_price(self):
+        return int(self.price * (1 - self.discount / 100))
+
+    def __str__(self):
+        return self.name_en
+    
+    def get_price_with_icon(self):
+        price = {
+            "USD": f"{self.get_price()} $",
+            "RUB": f"{self.get_price()} ₽",
+            "UZS": f"{self.get_price()} So'm",
+        }
+        return price[self.price_type]
+
+    def get_price_original_with_icon(self):
+        price = {
+            "USD": f"{self.price} $",
+            "RUB": f"{self.price} ₽",
+            "UZS": f"{self.price} So'm",
+        }
+        return price[self.price_type]
+    
+            
+    def get_created(self):
+        from django.utils.timezone import now
+        created = self.created
+        current = now()
+        diff = (current - created).total_seconds()
+
+        if diff < 60:
+            return "Now"
+        elif diff < 3600:
+            minutes = int(diff // 60)
+            return f"{minutes} minut oldin"
+        elif diff < 86400:
+            hours = int(diff // 3600)
+            return f"{hours} soat oldin"
+        else:
+            return created.strftime("%d.%m.%Y %H:%M")
+        
+        
+        
+    def get_updated(self):
+        from django.utils.timezone import now
+        updated = self.updated
+        current = now()
+        diff = (current - updated).total_seconds()
+
+        if diff < 60:
+            return "Hozir"
+        elif diff < 3600:
+            minutes = int(diff // 60)
+            return f"{minutes} minut oldin"
+        elif diff < 86400:
+            hours = int(diff // 3600)
+            return f"{hours} soat oldin"
+        else:
+            return updated.strftime("%d.%m.%Y %H:%M")
+        
+    
+    
+class ProductImg(models.Model):
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    img = models.ImageField(upload_to=product_image_upload_path)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING, related_name='product_image')
+
+
+class Basket(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quentity = models.IntegerField(default=0)
+    total_price = models.CharField(max_length=50, default='0')
+    
+    
+    def save(self, *args, **kwargs):
+        self.total_price = self.product.get_price() * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.user} → {self.product.name_ru} x {self.quantity}"
+    
+    
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishlist_items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="wishlisted_by")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "product") 
+
+    def __str__(self):
+        return f"{self.user} ❤️ {self.product}"
+
+
+class Promocode(models.Model):
+    status = models.BooleanField(default=True)
+    name = models.CharField(max_length=50)
+    discount = models.IntegerField(default=0)
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    quentity = models.IntegerField(default=0)
+    discount = models.IntegerField(default=0)
+    products = models.IntegerField(default=0)
+    
+class Order(models.Model):
+    order_item = models.ForeignKey(OrderItem, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, auto_created=False)
+    updated_at = models.DateField(auto_now_add=False, auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    promocode_id = models.ForeignKey(Promocode, on_delete=models.DO_NOTHING)
+
+
+class Contact(models.Model):
+    fio = models.CharField(default="Name Thername Father name")
+    phone = models.CharField(default='+998999999999')
+    message = models.TextField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
